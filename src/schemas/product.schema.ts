6916e2a7f394ac55af484da89bc41e63b5df12d9
MarceloCariz <marcejedd300@hotmail.com>
2023-05-18 13:49:24 -0400

    fix: createProductschema delete description is optional

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -5,9 +5,9 @@ export const createProductSchema = z.object({
     body: z.object({
         title: z.string().nonempty("El nombre del producto es requerido"),
     
-        description: z.string().nonempty("La descripcion es requerida")
-            .min(6, "La descripcion es demasiado corta")
-            .max(70, "La descripcion es demasiado larga"),
+        // description: z.string().nonempty("La descripcion es requerida")
+        //     .min(6, "La descripcion es demasiado corta")
+        //     .max(70, "La descripcion es demasiado larga"),
     
         price: z.string().refine((value) => {
             const numberValue = parseFloat(value);
